9dbde61ca289d1a8b84d4534dbde9ab20c60e826
Kevin Truong <kevintru0ng@yahooo.com>
2018-01-24 09:57:11 -0800

    day 2 battleship

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
+      <div className="header">Welcome to Battleship!</div>
         < Board />
       </div>
     );
--- src/components/board.js
+++ src/components/board.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
+const EMPTY = 0;
+const SHIP = null;
+const SHIP_HIT = "HIT!";
+const EMPTY_HIT = "MISS";
+
 class Board extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      board: []
+      board: [],
+      bgColor: 'lightblue',
+      torpedoes: 25
     }
     this.setUpBoard()
   }
@@ -16,70 +23,107 @@ class Board extends Component {
   }
 
   setUpBoard() {
-    for (let i = 0; i < 10; i++) {
-      this.state.board.push([])
+    let board = []
+
+    for(let row = 0; row < 10; row++) {
+      board.push([])
+      for(let col = 0; col < 10; col++) {
+        board[row][col] = EMPTY
+      }
     }
+    this.state.board = board
   }
 
+
+
   placeShip() {
-    let ship = 1;
     let i = 0;
-    // get the array out of the state
     let newBoard = this.state.board
 
     while(i < 1){
-      let randPosX = Math.floor(Math.random() * 10)
-      let randPosY = Math.floor(Math.random() * 10)
-      // update it
-      if(!newBoard[randPosX][randPosY] || newBoard[randPosX][randPosY].length === 0){ newBoard[randPosX][randPosY] = ship
+      let row = Math.floor(Math.random() * 10)
+      let col = Math.floor(Math.random() * 10)
+      if(!newBoard[row][col] || newBoard[row][col].length === 0) {
+        newBoard[row][col] = SHIP
         i++
       }
     }
-    // put it back with set state
     this.setState({
       board: newBoard
     })
   }
 
-  clickHandler(x, y) {
-    let newBoard = this.state.board
-    newBoard[y][x] = 6;
-    this.setState({
-      board: newBoard
-    })
+  clickHandler(row, col, e) {
+    const board = this.state.board
+
+    if (this.state.torpedoes === 0) {
+      alert('Game Over')
+
+    } else if (board[col][row] === SHIP) {
+      board[col][row] = SHIP_HIT
+      e.target.style.backgroundColor = 'green'
+      this.setState({
+        board: board,
+        torpedoes: this.state.torpedoes -= 1
+      })
+
+    } else if (board[col][row] === EMPTY) {
+      board[col][row] = EMPTY_HIT
+      e.target.style.backgroundColor = 'red'
+      this.setState({
+        board: board,
+        torpedoes: this.state.torpedoes -= 1
+      })
+
+    } else if (board[col][row] === SHIP_HIT || board[col][row] === EMPTY_HIT) {
+      alert("It's been HIT! Choose another location.")
+
+    } else {
+      this.setState({
+        board: board,
+        torpedoes: this.state.torpedoes -= 1
+      })
+    }
   }
 
-  renderRow(y) {
-    const row = [];
-    for (let x = 0; x < 10; x++) {
-      let theXY = x + "_" + y
-      row.push(
+  renderRow(row) {
+    const { board } = this.state
+
+    const set = [];
+    for (let col = 0; col < 10; col++) {
+      let theXY = col + "_" + row
+      set.push(
         <td id={theXY}
             key={theXY}
-            onClick={this.clickHandler.bind(this,x, y)}
+            onClick={this.clickHandler.bind(this, col, row)}
+            style={{backgroundColor:this.state.bgColor}}
+
         >
-          {this.state.board[y][x]}
+          {board[row][col] !== 0 ? board[row][col] : ''}
         </td>
       );
     }
-    return row;
+    return set;
   }
 
   renderRows() {
-      const rows = [];
-      for (let i = 0; i < 10; i++) {
-        rows.push(<tr key={i}>{this.renderRow(i)}</tr>);
-      }
-      return rows;
+    const rows = [];
+    for (let i = 0; i < 10; i++) {
+      rows.push(<tr key={i}>{this.renderRow(i)}</tr>);
+    }
+    return rows;
   }
 
   render() {
     return (
-      <table>
-        <tbody>
-          { this.renderRows() }
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <tbody>
+            { this.renderRows() }
+          </tbody>
+        </table>
+        <div className="torpedo">Torpedoes: {this.state.torpedoes}</div>
+      </div>
     )
   }
 }
