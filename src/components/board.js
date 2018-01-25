af3ee38a091aa21015d610f72b968770a7429f69
Kevin Truong <kevintru0ng@yahooo.com>
2018-01-24 16:46:52 -0800

    day 3 battelship

--- src/components/board.js
+++ src/components/board.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 const EMPTY = 0;
-const SHIP = null;
+const SHIP = "SHIP";
 const SHIP_HIT = "HIT!";
 const EMPTY_HIT = "MISS";
 
@@ -11,15 +11,14 @@ class Board extends Component {
     this.state = {
       board: [],
       bgColor: 'lightblue',
-      torpedoes: 25
+      torpedoes: 25,
+      battleships: 5
     }
     this.setUpBoard()
   }
 
-  componentWillMount(){
-    for(let i = 0; i < 5; i++){
-      this.placeShip()
-    }
+  componentWillMount() {
+    this.placeShips()
   }
 
   setUpBoard() {
@@ -34,23 +33,180 @@ class Board extends Component {
     this.state.board = board
   }
 
-
-
-  placeShip() {
-    let i = 0;
+  aircraftCarrierVertical() {
     let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 6)
+    let col = Math.floor(Math.random() * 10)
 
-    while(i < 1){
-      let row = Math.floor(Math.random() * 10)
-      let col = Math.floor(Math.random() * 10)
-      if(!newBoard[row][col] || newBoard[row][col].length === 0) {
-        newBoard[row][col] = SHIP
-        i++
-      }
+    if (!newBoard[row][col] && !newBoard[row+1][col] &&
+      !newBoard[row+2][col] && !newBoard[row+3][col] && !newBoard[row+4][col]) {
+      newBoard[row][col] = SHIP
+      newBoard[row+1][col] = SHIP
+      newBoard[row+2][col] = SHIP
+      newBoard[row+3][col] = SHIP
+      newBoard[row+4][col] = SHIP
+    } else {
+      this.aircraftCarrierVertical()
     }
-    this.setState({
-      board: newBoard
-    })
+  }
+
+  aircraftCarrierHorizontal() {
+    let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 10)
+    let col = Math.floor(Math.random() * 6)
+
+    if (!newBoard[row][col] && !newBoard[row][col+1] &&
+      !newBoard[row][col+2] && !newBoard[row][col+3] && !newBoard[row][col+4]) {
+      newBoard[row][col] = SHIP
+      newBoard[row][col+1] = SHIP
+      newBoard[row][col+2] = SHIP
+      newBoard[row][col+3] = SHIP
+      newBoard[row][col+4] = SHIP
+    } else {
+      this.aircraftCarrierHorizontal
+    }
+  }
+
+  uShipVertical() {
+    let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 10)
+    let col = Math.floor(Math.random() * 7)
+
+    if (!newBoard[row][col] && !newBoard[row][col+1] &&
+      !newBoard[row][col+2] && !newBoard[row][col+3]) {
+      newBoard[row][col] = SHIP
+      newBoard[row][col+1] = SHIP
+      newBoard[row][col+2] = SHIP
+      newBoard[row][col+3] = SHIP
+    } else {
+      this.uShipVertical()
+    }
+  }
+
+  uShipHorizontal() {
+    let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 6)
+    let col = Math.floor(Math.random() * 10)
+
+    if (!newBoard[row][col] && !newBoard[row][col+1] &&
+      !newBoard[row][col+2] && !newBoard[row][col+3]) {
+      newBoard[row][col] = SHIP
+      newBoard[row][col+1] = SHIP
+      newBoard[row][col+2] = SHIP
+      newBoard[row][col+3] = SHIP
+    } else {
+      this.uShipHorizontal()
+    }
+  }
+
+  submarineVertical() {
+    let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 10)
+    let col = Math.floor(Math.random() * 8)
+
+    if (!newBoard[row][col] && !newBoard[row][col+1] &&
+      !newBoard[row][col+2]) {
+      newBoard[row][col] = SHIP
+      newBoard[row][col+1] = SHIP
+      newBoard[row][col+2] = SHIP
+    } else {
+      this.submarineVertical()
+    }
+  }
+
+  submarineHorizontal() {
+    let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 7)
+    let col = Math.floor(Math.random() * 10)
+
+    if (!newBoard[row][col] && !newBoard[row][col+1] &&
+      !newBoard[row][col+2]) {
+      newBoard[row][col] = SHIP
+      newBoard[row][col+1] = SHIP
+      newBoard[row][col+2] = SHIP
+    } else {
+      this.submarineHorizontal()
+    }
+  }
+
+  cruiserVertical() {
+    let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 10)
+    let col = Math.floor(Math.random() * 9)
+
+    if (!newBoard[row][col] && !newBoard[row][col+1]) {
+      newBoard[row][col] = SHIP
+      newBoard[row][col+1] = SHIP
+    } else {
+      this.cruiserVertical()
+    }
+  }
+
+  cruiserHorizontal() {
+    let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 9)
+    let col = Math.floor(Math.random() * 10)
+
+    if (!newBoard[row][col] && !newBoard[row][col+1]) {
+      newBoard[row][col] = SHIP
+      newBoard[row][col+1] = SHIP
+    } else {
+      this.cruiserHorizontal()
+    }
+  }
+
+  patrolBoatVertical() {
+    let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 10)
+    let col = Math.floor(Math.random() * 10)
+
+    if (!newBoard[row][col]) {
+      newBoard[row][col] = SHIP
+    } else {
+      this.patrolBoatVertical()
+    }
+  }
+
+  patrolBoatHorizontal() {
+    let newBoard = this.state.board
+    let row = Math.floor(Math.random() * 10)
+    let col = Math.floor(Math.random() * 10)
+
+    if (!newBoard[row][col]) {
+      newBoard[row][col] = SHIP
+    } else {
+      this.patrolBoatHorizontal
+    }
+  }
+
+  placeShips() {
+    let newBoard = this.state.board
+    let coinFlip = Math.floor(Math.random() * 2)
+    if (coinFlip === 0) {
+      this.aircraftCarrierVertical()
+    } else {
+      this.aircraftCarrierHorizontal()
+    }
+    if (coinFlip === 0) {
+      this.uShipVertical()
+    } else {
+      this.uShipHorizontal()
+    }
+    if (coinFlip === 0) {
+      this.submarineVertical()
+    } else {
+      this.submarineHorizontal()
+    }
+    if (coinFlip === 0) {
+      this.cruiserVertical()
+    } else {
+      this.cruiserHorizontal()
+    }
+    if (coinFlip === 0) {
+      this.patrolBoatVertical()
+    } else {
+      this.patrolBoatHorizontal()
+    }
   }
 
   clickHandler(row, col, e) {
@@ -76,8 +232,8 @@ class Board extends Component {
       })
 
     } else if (board[col][row] === SHIP_HIT || board[col][row] === EMPTY_HIT) {
-      alert("It's been HIT! Choose another location.")
-
+      alert("Already shot here! Choose another location.")
+      console.log(board)
     } else {
       this.setState({
         board: board,
@@ -122,7 +278,8 @@ class Board extends Component {
             { this.renderRows() }
           </tbody>
         </table>
-        <div className="torpedo">Torpedoes: {this.state.torpedoes}</div>
+        <span className="torpedo">Torpedoes: {this.state.torpedoes}</span>
+        <span className="ships">Battleships: {this.state.battleships}</span>
       </div>
     )
   }
